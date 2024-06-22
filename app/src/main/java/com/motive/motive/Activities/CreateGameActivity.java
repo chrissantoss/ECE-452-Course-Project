400b691e39c0fe763bc2c6a2766b6d92ccb98fa9
Eimaan Ashraf <meashraf@uwaterloo.ca>
2024-06-22 17:21:53 -0400

    Added code to create game and ping firebase

--- app/src/main/java/com/motive/motive/Activities/CreateGameActivity.java
+++ app/src/main/java/com/motive/motive/Activities/CreateGameActivity.java
@@ -28,7 +28,7 @@ public class CreateGameActivity extends AppCompatActivity {
     private CheckBox genderFemale;
     private CheckBox genderNeutral;
     private CheckBox age16Under;
-    private CheckBox age17to36;
+    private CheckBox age17to36Input;
     private CheckBox age36Plus;
     private EditText notesInput;
     private Button createGameButton;
@@ -48,7 +48,7 @@ public class CreateGameActivity extends AppCompatActivity {
         genderFemale = findViewById(R.id.genderFemale);
         genderNeutral = findViewById(R.id.genderNeutral);
         age16Under = findViewById(R.id.age16Under);
-        age17to36 = findViewById(R.id.age17to36);
+        age17to36Input = findViewById(R.id.age17to36);
         age36Plus = findViewById(R.id.age36Plus);
         notesInput = findViewById(R.id.notesInput);
         createGameButton = findViewById(R.id.createGameButton);
@@ -58,7 +58,8 @@ public class CreateGameActivity extends AppCompatActivity {
 
 
     private void createGame() {
-        String gameType = gameTypeInput.getText().toString();
+//        String gameType = gameTypeInput.getText().toString();
+        String gameType = "Basketball";
         String gameSizeStr = gameSizeInput.getText().toString();
         String mandatoryItems = mandatoryItemsInput.getText().toString();
         String notes = notesInput.getText().toString();
@@ -68,5 +69,37 @@ public class CreateGameActivity extends AppCompatActivity {
             return;
         }
 
+        Integer gameSize = Integer.parseInt(gameSizeInput);
+        boolean beginner = experienceBeginner.isChecked();
+        boolean intermediate = experienceIntermediate.isChecked();
+        boolean expert = experienceExpert.isChecked();
+        boolean male = genderMale.isChecked();
+        boolean female = genderFemale.isChecked();
+        boolean neutral = genderNeutral.isChecked();
+        boolean age16 = age16Under.isChecked();
+        boolean age17to36 = age17to36Input.isChecked();
+        boolean age36 = age36Plus.isChecked();
+
+        String gameID = FirebaseFirestore.getInstance().collection("games").document().getId();
+        GameModel game = new GameModel(gameID, "hostID", 0.0, 0.0, gameSize, gameType);
+        game.setExperience(beginner, intermediate, expert);
+        game.setGenderPreference(male, female, neutral);
+        game.setAgePreference(age16, age17to36, age36);
+        game.setMandatoryItems(mandatoryItems);
+        game.setNotes(notes);
+
+        FirebaseFirestore.getInstance().collection("games").document(gameID)
+                .set(game)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateGameActivity.this, "Game Created Successfully", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else{
+                            Toast.makeText(CreateGameActivity.this, "Failed to create game", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
     }
 }
