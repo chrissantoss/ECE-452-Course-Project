0e9710258112780e426e80200ceeed1b19f60ab5
Michael Q <m7qian@uwaterloo.ca>
2024-06-25 01:43:37 -0400

    basic display games on map

--- app/src/main/java/com/motive/motive/Activities/HomePageActivity.java
+++ app/src/main/java/com/motive/motive/Activities/HomePageActivity.java
@@ -3,14 +3,24 @@ package com.motive.motive.Activities;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import com.motive.motive.R;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.motive.motive.Models.GameModel;
+import com.motive.motive.R;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,15 +29,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class HomePageActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap map;
     private LatLng userLocation;
+    private ArrayList<GameModel> games  = new ArrayList<GameModel>();
     private LatLng defaultLocation =  new LatLng(43.4723, -80.5449);
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        FirebaseFirestore.getInstance().collection("games").get().addOnSuccessListener(gamesDocuments ->{
+            for (DocumentSnapshot gameDocument : gamesDocuments.getDocuments()) {
+
+                GameModel game = gameDocument.toObject(GameModel.class);
+                if (game != null) {
+                    games.add(game);
+                }
+            }
+        });
 
         Button openCreateGameFormButton = findViewById(R.id.openCreateGameFormButton);
 
@@ -41,6 +65,8 @@ public class HomePageActivity extends AppCompatActivity {
             initializeMap();
         }
 
+
+
         openCreateGameFormButton.setOnClickListener(v -> {
             Intent intent = new Intent(HomePageActivity.this, CreateGameActivity.class);
             intent.putExtra("defaultLocation", defaultLocation);
@@ -53,13 +79,13 @@ public class HomePageActivity extends AppCompatActivity {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(googleMap -> {
             map = googleMap;
             map.setMyLocationEnabled(true);
 
             // Getting last known location
-            fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, location -> {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
                     if (location != null) {
                         // Move camera to user's location
                         Toast.makeText(this, "Location found", Toast.LENGTH_LONG).show();
@@ -68,8 +94,20 @@ public class HomePageActivity extends AppCompatActivity {
                     } else {
                         Toast.makeText(this, "Location not found", Toast.LENGTH_LONG).show();
                     }
-                });
+            });
+
+            for (GameModel game : games) {
+                Log.d("Firestore", "Adding marker at " +game.getLatitude()+", "+ game.getLongitude());
+                LatLng gameLocation = new LatLng(game.getLatitude(), game.getLongitude());
+                map.addMarker(new MarkerOptions().position(gameLocation).title(game.getGameType()));
+            }
+
+
+
         });
+
+        FirebaseFirestore.getInstance().collection("games").get();
+
     }
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
